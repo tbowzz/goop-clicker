463be6d1e98c570219ca728e683f01fb7360a86d
mepeter17 <michaelpeterson2@gmail.com>
2017-09-21 17:44:12 -0600

    Some Functionality
    Just wanted to push to let you guys know what I have so far, I'm pretty close to finishing the coding

--- GoopScript.js
+++
@@ -1,1 +0,0 @@
-GoopScript.js
---
+++ script.js
@@ -0,0 +1,113 @@
+var upgrade1_clicks = 1;
+var upgrade1_bought = false;
+
+var upgrade2_clicks = 2;
+var upgrade2_bought = false;
+
+var upgrade3_clicks = 3;
+var ship_bought = false;
+
+var upgrade4_clicks = 0;
+
+var OFF_OPACITY = 0.3;
+
+var score = 0;
+var score_multiplier = 1;
+
+var item1, item2, item3, item4, itemArray;
+var upgradeArray;
+function initialize()
+{
+	item1 = document.getElementById("buy_up1");
+	item1.innerHTML = "UPGRADE 1<br>buy for " + upgrade1_clicks;
+
+	item2 = document.getElementById("buy_up2");
+	item2.innerHTML  = "UPGRADE 2<br>buy for " + upgrade2_clicks;
+
+	item3 = document.getElementById("buy_ship")
+	item3.innerHTML  = "BUY SHIP<br>buy for " + upgrade3_clicks;
+
+	item4 = document.getElementById("fly_away");
+	item4.innerHTML  = "FLY AWAY<br>need ship ";
+
+	
+}
+window.onload = initialize;
+function clickFunction()
+{
+	score+=1*score_multiplier;
+	updateScoreText();
+	updateShop();
+}
+
+function updateScoreText()
+{
+	document.getElementById("score_place").innerHTML  = ("Evolution: " + score);
+}
+
+function updateShop()
+{
+	if(canBuy(1))
+	{
+		turnShopItemOn(item1);
+	}
+	else if(canBuy(2))
+	{
+		turnShopItemOn(item2);
+	}
+	else if(canBuy(3))
+	{
+		turnShopItemOn(item3);
+	}
+	else if(canBuy(4))
+	{
+		turnShopItemOn(item4);
+	}
+}
+
+function buy(number)
+{
+	if(canBuy(number))
+	{
+		turnShopItemOff(number);
+		switch(number)
+		{
+			case 1: 
+				upgrade1_bought = true;
+				score -= upgrade1_clicks;
+				break;
+			case 2:
+				upgrade2_bought = true;
+				score -= upgrade2_clicks;
+				break;
+			case 3:
+				ship_bought = true;
+				score -= upgrade3_clicks;
+		}
+		updateScoreText();
+
+	}
+}
+function canBuy(number)
+{
+	switch(number)
+	{
+		case 1: return score >= upgrade1_clicks && !upgrade1_bought;
+		case 2: return score >= upgrade2_clicks && !upgrade2_bought;
+		case 3: return score >= upgrade3_clicks && !ship_bought;
+		case 4: return ship_bought;
+	}
+	return false;
+}
+
+function turnShopItemOn(e)
+{
+	e.style.opacity = 1.0;
+	e.style.backgroundColor = "lightgreen";
+}
+
+function turnShopItemOff(e)
+{
+	e.style.opacity = OFF_OPACITY;
+	e.style.backgroundColor = "lightblue";
+}
